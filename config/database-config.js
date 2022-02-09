61802e750a4914d02f5627b9efb4b37d461779db
gribdesbois <gribdesbois@gmail.com>
2022-02-09 22:15:50 +0100

    migrating dialect

--- config/database-config.js
+++ config/database-config.js
@@ -34,13 +34,29 @@ const db = new Sequelize(/* {
   },
 } */
   DATABASE_URL, {
-    dialect: 'pg',
+    dialect: 'postgres',
     /* port: 5432, */
     dialectOptions: { //! no pg_a_..conf otherwise
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
+    },
+    pool: {
+      connectionLimit: 5,
+      // 'connectTimeout' is the maximum number of milliseconds before a timeout
+      // occurs during the initial connection to the database.
+      connectTimeout: 10000, // 10 seconds
+      // 'acquireTimeout' is the maximum number of milliseconds to wait when
+      // checking out a connection from the pool before a timeout error occurs.
+      acquireTimeout: 10000, // 10 seconds
+      // 'waitForConnections' determines the pool's action when no connections are
+      // free. If true, the request will queued and a connection will be presented
+      // when ready. If false, the pool will call back with an error.
+      waitForConnections: true, // Default: true
+      // 'queueLimit' is the maximum number of requests for connections the pool
+      // will queue at once before returning an error. If 0, there is no limit.
+      queueLimit: 0, // Default: 0
     },
   },
 )
