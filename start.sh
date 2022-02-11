60390da41f85726a1280608640781a41e03de1f4
gribdesbois <gribdesbois@gmail.com>
2022-02-11 02:49:39 +0100

    typos

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$ENABLE_DEBUG" == "true" ]; then
 	echo "Starting with debugger on port $AUX_PORT"
-	exec with_ngrok node --debug=$AUX_PORT server.js
+	exec with_ngrok node --inspect=$AUX_PORT server.js
 else
 	echo "Starting without debugger"
 	exec node server.js
