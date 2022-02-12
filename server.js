57fa6a022561a5f438ae7fd1d87445f7bf518c74
gribdesbois <gribdesbois@gmail.com>
2022-02-12 03:12:11 +0100

    port 443

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const normalizePort = (val) => {
   }
   return false
 }
-const port = normalizePort(process.env.PORT || '3000')
+const port = normalizePort(process.env.PORT || '443')
 app.set('port', port)
 
 const errorHandler = (error) => {
